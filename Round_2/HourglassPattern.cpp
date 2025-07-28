6517658124f5b192a8c9b7bd954dc8ca034f0745
Vijitverm1234 <143703788+Vijitverm1234@users.noreply.github.com>
2025-07-28 20:25:09 +0530

    round 2

--- Round_2/HourglassPattern.cpp
+++ Round_2/HourglassPattern.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-  
+  int row=7;
+  for(int i=1;i<=7;i++){
+    for(int k=1;k<i;k++){
+        cout<<" ";
+    }
+    for(int j=row-i+1;j>=1;j--){
+        cout<<" * ";
+    }
+    cout<<endl;
+  }
+  for(int i=2;i<=row;i++){
+    for(int k=row-i;k>=1;k--){
+        cout<<" ";
+    }
+    for(int j=1;j<=i;j++){
+        cout<<" * ";
+    }
+    cout<<endl;
+  }
 }
